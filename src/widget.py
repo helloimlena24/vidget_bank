80f72b9ea6bd3afbb014afbe906f1f7937a74f90
elena.agafonova <helloimlena@yandex.ru>
2024-12-09 12:39:08 +0300

    update lint src

--- src/widget.py
+++ src/widget.py
@@ -1,10 +1,14 @@
 from src.masks import get_mask_account, get_mask_card_number
+
+
 def mask_account_card(inf_card: str) -> str:
-    if "Счет" in  inf_card:
+    """Функция маскирует информацию и о картах, и о счетах"""
+    if "Счет" in inf_card:
         return f"{inf_card[:4]} {get_mask_account(inf_card[-4:])}"
     else:
         return f"{inf_card[:-17]} {get_mask_card_number(inf_card[-16:])}"
 
 
-def get_date(date:str) -> str:
+def get_date(date: str) -> str:
+    """Функция возвращает строку с датой в формате "ДД.ММ.ГГГГ" """
     return f"{date[8:10]}.{date[5:7]}.{date[:4]}"
